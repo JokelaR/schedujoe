07a76c08bc92dec6ca63c5e5734414f940a9b4ac
Bulder <buldersecondary@gmail.com>
2023-04-02 05:50:56 +0300

    new secret leap day rules added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const hypnospaceStyle = document.getElementById("hypnospaceStyle");
+const umineko = document.getElementById("Umineko");
+let umiCounter = 1;
 
 function toggleHidden(elementIDs) {
     elementIDs.forEach(elementID => {
@@ -18,4 +20,13 @@ function toggleCollapsed(sourceElement) {
 
 function toggleHypnospace() {
     hypnospaceStyle.disabled = !hypnospaceStyle.disabled;
+}
+
+function incrementUmineko() {
+    if(umiCounter >= 12 ) {
+        umineko.classList.toggle('hidden');
+    }
+    else {
+        umiCounter = umiCounter + 1;
+    }
 }
