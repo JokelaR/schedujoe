46ab7fa6d6a6839c181f3d4365e7d2597d149220
Romi Jokela <jokelaromi@gmail.com>
2024-12-16 03:29:42 +0200

    Maybe make logo rendering consistent

--- html_chunks.py
+++ html_chunks.py
@@ -11,7 +11,6 @@ html_start = """
 head = """
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Schedujoe - The Joseph Schedule</title>
         <meta name="description" content="The (non-)authoritative source for the next planned Joseph Anderson stream games.">
